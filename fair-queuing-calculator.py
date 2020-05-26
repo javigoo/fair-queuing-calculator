d9927db4112164423d60659d2aad000da6e7ea9c
javigoo <javierroiggregorio@gmail.com>
2020-05-27 01:47:54 +0200

    Fix bug

--- fair-queuing-calculator.py
+++ fair-queuing-calculator.py
@@ -5,14 +5,29 @@ import os
 
 def parse(file):
     paquets = []
-    print("Taula de paquets:\n")
+    print("\nTabla de %s\n" % sys.argv[1])
+    print("Paquete\tLlegada\tMedida\tFlujo")
     for line in open(file):
-        print(line)
-
-        paquets.append(line.split())
+        package_data = line.split()
+        print(package_data[0],"\t",package_data[1],"\t",package_data[2],"\t",package_data[3])
+        paquets.append(package_data)
     print()
+    
     return paquets
 
+def parse_flujo(flujos):
+    flow_weight = {}
+    flow_data = flujos.split(",")
+    for flow in flow_data:
+        flow = flow.split(":")
+        if(len(flow[1]) == 3):
+            weight = flow[1].split("/")[1]
+        else:
+            weight = flow[1]
+
+        flow_weight[flow[0]] = weight
+
+    return flow_weight
 
 def select_less_time_to_complete(paquetes):
     paquet_menor_temps = paquetes[0]
@@ -25,9 +40,8 @@ def select_less_time_to_complete(paquetes):
     
     return paquet_menor_temps
 
-def fair_queuing(paquets, temps_finalitzacio_segment=0, paquets_rebuts = [], package_transmission_order = []):
+def fair_queuing(paquets, temps_finalitzacio_segment=0, paquets_rebuts = [], package_transmission_order = [], flow = {}):
 
-    # Tiempo de finalizacion con el servidor libre
     for paquet in paquets:
         temps_arribada = int(paquet[1])
         mida = int(paquet[2])
@@ -42,29 +56,43 @@ def fair_queuing(paquets, temps_finalitzacio_segment=0, paquets_rebuts = [], pac
             paquets.remove(paquete[:-1])
 
     paquete_recibido = select_less_time_to_complete(paquets_rebuts)
-    temps_finalitzacio_segment += paquete_recibido[4]
+    tiempo_paquete = paquete_recibido[4]
+    medida_paquete = int(paquete_recibido[2])
 
+    temps_finalitzacio_segment += medida_paquete
     paquets_rebuts.remove(paquete_recibido)
 
-    print("*** El paquete",paquete_recibido[0], "llega con un tiempo de",temps_finalitzacio_segment,"***")
+    print("*** El paquete",paquete_recibido[0], "llega con un tiempo de",tiempo_paquete,"***")
 
     package_transmission_order.append(paquete_recibido[0])
 
     if len(paquets_rebuts) == 0:
-        return print("\nOrden de enviamiento de los paquetes", package_transmission_order)
+        return print("\nOrden de trasmision: ", ",".join(package_transmission_order))
 
     fair_queuing(paquetes, temps_finalitzacio_segment ,paquets_rebuts, package_transmission_order)
 
+def weighted_fair_queuing(paquetes, flujo):
+    fair_queuing(paquets = paquetes, flow = {})
+
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        sys.exit("Use: %s <taula_paquets>" % sys.argv[0])
+    print(len(sys.argv))
+
+    if len(sys.argv) <= 1:
+        sys.exit("Use: %s <tabla_paquetes>" % sys.argv[0])
 
     if os.path.isfile(sys.argv[1]):
         taula_paquets_file = os.path.abspath(sys.argv[1])
     else:
-        sys.exit("ERROR: Taula paquets not found (%s)" % sys.argv[1])
+        sys.exit("Error! %s not found" % sys.argv[1])
 
     paquetes = parse(taula_paquets_file)
+    
+    if len(sys.argv) >= 3:
+        if sys.argv[2]=="-w" and len(sys.argv) == 4:
+            flujos = parse_flujo(sys.argv[3])
+            weighted_fair_queuing(paquetes, flujos)
+        else:
+            sys.exit("Use: %s <tabla_paquetes> [-w <flujo1:ancho de banda1,flujo2:ancho de banda2...>]" % sys.argv[0])
 
     fair_queuing(paquetes)
